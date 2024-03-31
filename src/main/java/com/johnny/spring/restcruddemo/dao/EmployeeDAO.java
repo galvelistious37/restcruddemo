48909fbd93439594ed792333eb1aa46f6a031684
johnny <john.robertson88@yahoo.com>
2024-03-30 21:41:31 -0700

    Add the rest of the crud method to EmployeeDAOImpl

--- src/main/java/com/johnny/spring/restcruddemo/dao/EmployeeDAO.java
+++ src/main/java/com/johnny/spring/restcruddemo/dao/EmployeeDAO.java
@@ -7,4 +7,10 @@ import java.util.List;
 public interface EmployeeDAO {
 
     List<Employee> findAll();
+
+    public Employee findById(int id);
+
+    public Employee save(Employee employee);
+
+    public void deleteById(int id);
 }
--- src/main/java/com/johnny/spring/restcruddemo/dao/EmployeeDAOImpl.java
+++ src/main/java/com/johnny/spring/restcruddemo/dao/EmployeeDAOImpl.java
@@ -31,4 +31,26 @@ public class EmployeeDAOImpl implements EmployeeDAO{
         // Return the results
         return theQeury.getResultList();
     }
+
+    @Override
+    public Employee findById(int id) {
+        // Get employee
+        // Return employee
+        return entityManager.find(Employee.class, id);
+    }
+
+    @Override
+    public Employee save(Employee employee) {
+        // Merge will save or update...
+        // if id = 0, it inserts... else, update
+        return entityManager.merge(employee);
+    }
+
+    @Override
+    public void deleteById(int id) {
+        // Find employee by id
+        Employee employee = findById(id);
+        // Delete the employee
+        entityManager.remove(employee);
+    }
 }
