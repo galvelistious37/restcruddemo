484c636c4226dbb6537a486b8c74d2c6b0fa1d42
johnny <john.robertson88@yahoo.com>
2024-03-31 16:43:48 -0700

    Add Spring Data REST dependency to the pom.xml file and remove EmployeeController, EmployeeService, and EmployeeServiceImpl because Spring Data REST provides all endpoints without requiring manual coding of boilerplate endpoint configuration

---
+++ src/main/java/com/johnny/spring/restcruddemo/notneeded/service/EmployeeService.java
@@ -0,0 +1,21 @@
+//package com.johnny.spring.restcruddemo.notneeded.service;
+//
+//import com.johnny.spring.restcruddemo.entity.Employee;
+//
+//import java.util.List;
+//
+/**
+ * No longer needed when importing spring-boot-starter-data-rest. Spring Date
+ * REST provides endpoints and we don't need to manually create them anymore.
+ */
+
+//public interface EmployeeService {
+//
+//    List<Employee> findAll();
+//
+//    public Employee findById(int id);
+//
+//    public Employee save(Employee employee);
+//
+//    public void deleteById(int id);
+//}
